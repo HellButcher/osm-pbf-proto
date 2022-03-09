b3396f623b2a8427928dd207769e5f5a3df3ff10
Christoph Hommelsheim <HellButcher@users.noreply.github.com>
2022-03-09 15:39:27 +0100

    include generated module, rerun if changed

--- build.rs
+++ build.rs
@@ -1,8 +1,13 @@
+const PROTOS: &[&str] = &["src/protos/fileformat.proto", "src/protos/osmformat.proto"];
+
 fn main() {
+    for proto in PROTOS {
+        println!("cargo:rerun-if-changed={}", proto);
+    }
     protobuf_codegen::Codegen::new()
         .pure()
         .cargo_out_dir("protos-gen")
         .includes(&["src/protos"])
-        .inputs(&["src/protos/fileformat.proto", "src/protos/osmformat.proto"])
+        .inputs(PROTOS)
         .run_from_script();
 }
